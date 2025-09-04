afc674499cd75b2f6963670abc9a5a2a423b6913
harsho-afk <rharshith247@gmail.com>
2025-09-05 01:00:00 +0530

    Auto-sync dotfiles

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+-- ~/.config/nvim/lua/plugins/colorscheme.lua
+return {
+    "rebelot/kanagawa.nvim",
+    lazy = false,       -- load during startup
+    priority = 1000,    -- load before other plugins
+    config = function()
+        vim.cmd("colorscheme kanagawa")
+    end,
+}
+
