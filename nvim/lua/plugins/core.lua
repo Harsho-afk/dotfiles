672bdb6e9e2986c3e348a03747bf6c9bbe071446
harsho-afk <rharshith247@gmail.com>
2025-09-11 18:00:00 +0530

    Auto-sync dotfiles

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -151,7 +151,23 @@ return {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function() 
-            local builtin = require('telescope.builtin')
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            local builtin = require("telescope.builtin")
+            require('telescope').setup{
+                defaults = {
+                    mappings = {
+                        i = { 
+                            ["<C-q>"] = actions.close,
+                        },
+                    }
+                },
+                pickers = {
+                },
+                extensions = {
+                }
+            }
+
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
