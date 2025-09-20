6a2aacacb8d233f609acbeca75c7244a8817cb84
harsho-afk <rharshith247@gmail.com>
2025-09-20 16:32:55 +0530

    Auto-sync dotfiles

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,7 +11,6 @@ return {
         dependencies = "nvim-lua/plenary.nvim",
         config = function()
             require("diffview").setup()
-            -- Diffview keymaps
             vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>",
                 { noremap = true, silent = true, desc = "Open Diffview" })
             vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>",
