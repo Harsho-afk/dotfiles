2c54a9b013b6e18b1e041d50fd0b61b0347daebd
harsho-afk <rharshith247@gmail.com>
2025-09-20 16:31:11 +0530

    Auto-sync dotfiles

--- autosync.sh
+++ autosync.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-cp -ru ~/.config/hypr ~/.config/nvim ~/.config/waybar ~/Programms/dotfiles
-cd ~/Programms/dotfiles
-git add .
+rsync -av --delete \
+    ~/.config/hypr \
+    ~/.config/nvim \
+    ~/.config/waybar \
+    ~/.config/kitty \
+    ~/Programms/dotfiles/
+
+cd ~/Programms/dotfiles || exit
+git add -A
 git commit -m "Auto-sync dotfiles"
 git push -u origin main
