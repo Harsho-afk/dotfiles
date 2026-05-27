452d1e3f920edc05a5093301581c562ed587ab45
harsho-afk <rharshith247@gmail.com>
2026-05-27 18:00:00 +0530

    Auto-sync dotfiles

---
+++ hypr/env.lua
@@ -0,0 +1,6 @@
+hl.env("AQ_DRM_DEVICES","/dev/dri/intel-igpu:/dev/dri/nvidia-gpu")
+hl.env("XCURSOR_SIZE",24)
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("NVD_BACKEND","direct")
+hl.env("QT_QPA_PLATFORMTHEME","qt6ct")
+
--- hypr/hyperland.lua
+++
@@ -1,3 +0,0 @@
-local home_dir = os.getenv("HOME")
-
-hl.env("AQ_DRM_DEVICES","/dev/dri/intel-igpu:/dev/dri/nvidia-gpu")
